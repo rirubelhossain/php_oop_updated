694fb90f624c879672ba2f1454f8b0541aca770b
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2022-03-08 00:08:22 +0600

    Basic oop class and object

--- index.php
+++ index.php
@@ -2,49 +2,42 @@
 
 
 
-
 <?php 
 
-
- class Person{
-    public $name;
-    public $age;
-
-    public function personName(){
-      echo "Person Name: ".$this->name;
-    }
- 
-    public function personAge($age){
-    echo $this->age = $age;
-    }
-
+class Person{
+	public $name ;
+	public $age ;
+	
+	public function nationality($data){
+		echo "The Natinonality is ". $data ."<br>";
+	}
+	public function set_properties($name , $age){
+		$this->name = $name ;
+		$this->age = $age ;
+	}
+	public function print_pro(){
+		echo "The name is = ".$this->name ."<br>";
+		echo "And age is = ".$this->age ."<br>";
+	}
 }
+/// create object type variable here below
+$rubel = new Person() ;
+$rubel->nationality("Bangladeshi");
+$rubel->set_properties("Rubel Hossain"," 28 ");
+$rubel->print_pro();
 
-$personOne = new Person;
-$personOne->name = "Akbar Hossain";
-$personOne->personName();
-echo "<br>";
+echo "<br><br>";
 
-$personOne->personAge("28");
-
-
+ ///create again object variable 
+ $rasel = new Person() ;
+ $rasel->nationality("Bangladeshi");
+ $rasel->set_properties("Rasel Hossain"," 26 ");
+ $rasel->print_pro();
+ 
+ 
 
 ?>
 
 		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include "inc/footer.php"; ?>
